ce9b0c47db37a030edb994d6db2bc224d23ae0d0
absaGH <absa3852@gmail.com>
2021-11-24 16:05:53 +0000

    ES6 Promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
   promise1.then((result) => ({ status: 'satisfied', value: result }));
   promise2.catch((error) => ({ status: 'failed', value: error.toString() }));
-  return Promise.race([promise1, promise2]);
+  return Promise.resolve([promise1, promise2]);
 }
