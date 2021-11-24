459784eb5635ef0ebb4562f678eabac1050a627e
absaGH <absa3852@gmail.com>
2021-11-24 16:07:13 +0000

    ES6 Promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
   promise1.then((result) => ({ status: 'satisfied', value: result }));
