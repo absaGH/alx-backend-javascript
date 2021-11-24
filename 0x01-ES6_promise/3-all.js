1cb551a2ae00a94e9cfb73204c1221fd18443e9f
absaGH <absa3852@gmail.com>
2021-11-24 14:35:12 +0000

    ES6 Promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
-  Promise.all([promise1, promise2]).then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`));
+  const allPromises = Promise.all([promise1, promise2]);
+  allPromises
+    .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`))
+    .catch(() => new Error('Signup system offline'));
 }
