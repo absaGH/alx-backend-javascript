92f840eee8ff3227789a6a87f3dce2c92c4a185c
absaGH <absa3852@gmail.com>
2021-11-26 08:54:46 +0000

    ES6 basic

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const modifiedarray = [];
   for (let idx of array) {
     const value = idx;
     idx = appendString + value;
+    modifiedarray.push(idx);
   }
 
-  return array;
+  return modifiedarray;
 }
