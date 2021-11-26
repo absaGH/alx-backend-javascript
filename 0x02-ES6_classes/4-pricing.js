22d22e9737fb66fcc06aa9251754c816122c5eff
absaGH <absa3852@gmail.com>
2021-11-26 15:35:02 +0000

    ES6 Classes

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(am) {
+    this._amount = am;
+  }
+
+  set currency(cur) {
+    if (!(cur instanceof Currency)) throw TypeError('currency must be an instance of Currency');
+    this._currency = cur;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  convertPrice(amount, conversionRate) {
+    return this._amount * conversionRate;
+  }
+}
