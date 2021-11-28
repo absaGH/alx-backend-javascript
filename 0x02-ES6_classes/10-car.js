0a824c58ae26655b984bff802ebe27b82fa6bca3
absaGH <absa3852@gmail.com>
2021-11-28 11:42:54 +0000

    ES6 Classes

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const hasSpecies = this.constructor[Symbol.species];
+    const newClone = hasSpecies ? new this.constructor[Symbol.species]() : new this.constructor();
+    return newClone;
+  }
+}
