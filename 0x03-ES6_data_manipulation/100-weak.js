e33f4ecdd249ce30d90e337158fd57315572d3db
absaGH <absa3852@gmail.com>
2021-11-30 14:33:50 +0000

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,7 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
+    weakMap.set(endpoint, 0);
   } else if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   } else {
