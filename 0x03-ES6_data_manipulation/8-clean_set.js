73ed5d994c9b83ff6b076e0caa98f4c757223579
absaGH <absa3852@gmail.com>
2021-12-01 04:54:21 +0000

    ES6 Data Manipulaion

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,15 @@
 export default function cleanSet(set, startString) {
   let str = '';
-  if (startString) {
-    set.forEach((item) => {
-      if ((typeof startString === 'string') && item.startsWith(startString)) {
+  if (startString && typeof startString === 'string') {
+    for (const item of set) {
+      if ((typeof item === 'string') && item.startsWith(startString)) {
         if (str === '') {
           str = str.concat(item.substring(startString.length));
         } else {
           str = str.concat('-', item.substring(startString.length));
         }
       }
-    });
+    }
     return str;
   }
   return '';
