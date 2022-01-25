1fb606e56a053f65db1ac038ab46c3bb70cc0119
absaGH <absa3852@gmail.com>
2022-01-25 12:16:25 +0000

    process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}\n`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
