9b34737e85c43fe878fc5ea5283cf5a8cac65ae4
absaGH <absa3852@gmail.com>
2022-01-26 15:28:27 +0000

    simple http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,10 @@
-const app = require('http');
+const http = require('http');
 
 const requestListener = (req, res) => {
   res.writeHead(200);
   res.end('Hello Holberton School!');
 };
 
-const server = app.createServer(requestListener);
-server.listen(1245);
+const app = http.createServer(requestListener);
+app.listen(1245);
+module.exports = app;
