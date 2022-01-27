532cdccbb5c8770215367b5a3d07fef25a692cd8
absaGH <absa3852@gmail.com>
2022-01-27 11:14:49 +0000

    simple http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -6,11 +6,11 @@ const pth = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello Holberton School!');
     res.end();
   } else if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
     try {
       const data = await countStudents(pth);
