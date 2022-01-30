624a0f314a4caf0c156d0054a3b75303e9a7bb0a
absaGH <absa3852@gmail.com>
2022-01-30 15:11:51 +0000

    testing using chai

--- 0x06-unittests_in_js/2-calcul.test.js
+++ 0x06-unittests_in_js/2-calcul.test.js
@@ -6,31 +6,31 @@ const expect = chai.expect;
 
 describe('calculateNumber Sum', () => {
     it('check result', () => {
-        expect(calculateNumber('SUM', 1, 3)).to.equal(4);
-        expect(calculateNumber('SUM', 1.2, 3)).to.equal(4);
-        expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
-        expect(calculateNumber('SUM', 4.5, 1.4)).to.equal(6);
-        expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
-        expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
+        chai.expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+        chai.expect(calculateNumber('SUM', 1.2, 3)).to.equal(4);
+        chai.expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+        chai.expect(calculateNumber('SUM', 4.5, 1.4)).to.equal(6);
+        chai.expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+        chai.expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
     });
 });
 
 describe('calculateNumber Subtract', () => {
     it('check result', () => {
- 	expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-        expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
-        expect(calculateNumber('SUBTRACT', 1.2, 1.2)).to.equal(0);
-        expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
-        expect(calculateNumber('SUBTRACT', 4.1, 1)).to.equal(3);
+ 	chai.expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+        chai.expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
+        chai.expect(calculateNumber('SUBTRACT', 1.2, 1.2)).to.equal(0);
+        chai.expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
+        chai.expect(calculateNumber('SUBTRACT', 4.1, 1)).to.equal(3);
     });
 });
 
 describe('calculateNumber Divide', () => {
     it('check result', () => {
-        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-	expect(calculateNumber('DIVIDE', 1, 4.5)).to.equal(0.2);
-	expect(calculateNumber('DIVIDE', 0.0, 1)).to.equal(0);
-	expect(calculateNumber('DIVIDE', -4, 2)).to.equal(-2);
-	expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+        chai.expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+	chai.expect(calculateNumber('DIVIDE', 1, 4.5)).to.equal(0.2);
+	chai.expect(calculateNumber('DIVIDE', 0.0, 1)).to.equal(0);
+	chai.expect(calculateNumber('DIVIDE', -4, 2)).to.equal(-2);
+	chai.expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
 });
