3e786df4450f001bef2d1e6cb0ea812c8a66105e
absaGH <absa3852@gmail.com>
2022-01-30 12:33:35 +0000

    assert equal test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,9 +4,10 @@ const assert = require("assert");
 
 describe('calculateNumber', () => {
     it('check result', () => {
-	assert.equal(calculateNumber(1, 3), 4);
-	assert.equal(calculateNumber(1, 3.7), 5);
-	assert.equal(calculateNumber(1.2, 3.7), 5);
-	assert.equal(calculateNumber(1.5, 3.7), 6);
+	assert.strictEqual(calculateNumber(1, 3), 4);
+	assert.strictEqual(calculateNumber(1.2, 3), 4);
+	assert.strictEqual(calculateNumber(1, 3.7), 5);
+	assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+	assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
