bfdc97db4a4c2986609eeb9eeab5c442625ae4e2
absaGH <absa3852@gmail.com>
2022-01-30 13:32:24 +0000

    basic testing using mocha

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,9 +4,23 @@ const assert = require("assert");
 
 describe('calculateNumber', () => {
     it('check result', () => {
-        assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+        assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+        assert.strictEqual(calculateNumber('SUM', 1.2, 3), 4);
+        assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+        assert.strictEqual(calculateNumber('SUM', 4.5, 1.4), 6);
+        assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+	
         assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        assert.strictEqual(calculateNumber('SUBTRACT', 1, 3), -2);
+        assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 1.2), 0);
+        assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), -3);
+        assert.strictEqual(calculateNumber('SUBTRACT', 4.1, 1), 3);
+        	
         assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+	assert.strictEqual(calculateNumber('DIVIDE', 1, 4.5), 0.2);
+	assert.strictEqual(calculateNumber('DIVIDE', 1.5, 1), 2);
+	assert.strictEqual(calculateNumber('DIVIDE', 4, 2), 2);
+	assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
 });
