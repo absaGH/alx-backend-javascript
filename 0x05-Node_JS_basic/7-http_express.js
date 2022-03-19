5f38879d99c5fa138744befa821b88c36f8d4270
absaGH <absa3852@gmail.com>
2022-03-19 13:19:37 +0000

    http server using express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const countStudents = require('./3-read_file_async');
 
-const pth = process.argv[2];
+const args = process.argv.slice(2);
+const DATABASE = args[0];
 
 const app = express();
 
@@ -10,16 +11,15 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.route('/students')
-  .get(async (req, res) => {
-    const tag = 'This is the list of our students\n';
-    try {
-      const data = await countStudents(pth);
-      res.send(`${tag}${data.join('\n')}`);
-    } catch (error) {
-      res.status(404).send(error.message);
-    }
-  });
+app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
+  }
+});
 
 app.listen(1245);
 
